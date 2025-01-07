3cd151d94146646158261ad9587f60790e268429
z-rivald <127426656+z-rivald@users.noreply.github.com>
2025-01-07 09:04:54 +0500

    Create mysplit.py

---
+++ mysplit.py
@@ -0,0 +1,24 @@
+def mysplit(strng):
+    result = []
+    j = 0 #variable to set a starting point
+    new_str = ''
+    for i in range(len(strng)):
+        if ord(strng[i]) != 32:
+            new_str += strng[i]
+        elif ord(strng[i]) == 32 and len(new_str) != 0:
+            result.append(new_str)
+            new_str = ''
+        if i == len(strng)-1 and len(new_str) != 0:
+            result.append(new_str) 
+    return result
+
+    #
+    # put your code here
+    #
+
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
